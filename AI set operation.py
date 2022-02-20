4e0ae5316751387ab0886bb9b35477e38b9519bd
Suraj KK <surajkannan77@gmail.com>
2022-02-20 20:27:08 +0530

    Create AI set operation.py

---
+++ AI set operation.py
@@ -0,0 +1,14 @@
+E = {0, 2, 4, 6, 10};
+N = {1, 2, 3, 5, 10};
+
+# set union
+print("Union of E and N is",E | N)
+
+# set intersection
+print("Intersection of E and N is",E & N)
+
+# set difference
+print("Difference of E and N is",E - N)
+
+# set symmetric difference
+print("Symmetric difference of E and N is",E ^ N)
